b9979441a8f206684e7d3290e62bae224a952d74
Jinxwp <mikemahnic@gmail.com>
2025-07-07 19:14:56 -0400

    adding in some files

---
+++ main.py
@@ -0,0 +1,56 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+from player import Player
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    # Create a window from width and height
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    #Look at this! a Clock for our game
+    clock = pygame.time.Clock()
+    
+    #make some groups before the variables
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    #just like groups we need the container values above the variables they use
+    Player.containers = (updateable, drawable)
+
+    #make a player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
+    
+    #the speed delta
+    dt = 0
+        
+    # The magic goes here
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        updateable.update(dt)
+        
+        screen.fill("black")
+        
+        #lets make sure we draw all the stuff in the drawable container
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+        
+        # Making sure our frames don't go past 60 per loop. Thanks clock!
+        dt = clock.tick(60) / 1000
+        
+    
+if __name__ == "__main__":
+    main()
+
+   
